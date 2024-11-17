1a4a303ad50737f6a635e9d075764d2c3d0cd8e0
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2024-11-16 19:40:24 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.0</p>');
+})
+
+app.get('/products', (req, res) => {
+    res.send([
+        {
+            id: 101,
+            name: 'Product 1',
+            price: 100
+        },
+        {
+            id: 102,
+            name: 'Product 2',
+            price: 200
+        }
+    ])
+})
+
+app.listen(port, () => {
+    console.log(`Demo app is up and listening to port: ${port}`)
+})
